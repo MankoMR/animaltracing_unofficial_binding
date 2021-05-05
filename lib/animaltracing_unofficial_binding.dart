69322d8ad8d37757c81f21d48a53014e2332c1a2
MankoMR <manuel.koloska@gmail.com>
2021-05-05 17:39:58 +0200

    Remove unnecessary files

--- lib/animaltracing_unofficial_binding.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library animaltracing_unofficial_binding;
-
-export 'src/animaltracing_unofficial_binding_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
