b6aaefded1cb1a2ab5df92edcc1d49f20ad1ee8a
MankoMR <manuel.koloska@gmail.com>
2021-05-06 14:53:59 +0200

    Add XmlMissingElementException

---
+++ lib/exceptions/xml_missing_element_exception.dart
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Manuel Koloska. All Rights reserved.
+ * Filename: xml_missing_element_exception.dart
+ * Project: animaltracing_unofficial_binding.
+ */
+
+import 'library_exception.dart';
+
+class XmlMissingElementException implements LibraryException {
+  final String elementName;
+  final String? nameSpace;
+  final String? message;
+
+  XmlMissingElementException(this.elementName, this.nameSpace, this.message);
+}
