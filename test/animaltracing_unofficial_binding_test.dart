f107cfbef5c4ebda5c597b426c3ae09d9d360c71
MankoMR <manuel.koloska@gmail.com>
2021-05-06 10:26:18 +0200

    Fix all isssues with code.

--- test/animaltracing_unofficial_binding_test.dart
+++ test/animaltracing_unofficial_binding_test.dart
@@ -1,14 +1,1 @@
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    setUp(() {});
-
-    test('Failing Test', () {
-	
-	
-      expect(false, true);
-      final Test =        "";
-    });
-  });
-}
+void main() {}
