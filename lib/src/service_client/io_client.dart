3ffa273b47b5a102963cc1c1755db672ed10c71a
MankoMR <manuel.koloska@gmail.com>
2021-05-06 17:21:35 +0200

    Implement IoClient

--- lib/src/service_client/io_client.dart
+++ lib/src/service_client/io_client.dart
@@ -3,6 +3,13 @@
  * Filename: io_client.dart
  * Project: animaltracing_unofficial_binding.
  */
+import 'dart:convert';
+import 'dart:io';
+
+import '../../exceptions/http_exception.dart';
+import '../../exceptions/library_exception.dart';
+import '../../exceptions/soap_exception.dart';
+import '../../exceptions/string_decoding_exception.dart';
 import '../soap/soap_request.dart';
 import '../soap/soap_response.dart';
 import 'base_client.dart';
@@ -12,8 +19,63 @@ class IOClient extends BaseClient {
 
   @override
   Future<SoapResponse> sendRequest(
-      SoapRequest soapRequest, String authorizationToken) {
+      SoapRequest soapRequest, String authorizationToken) async {
+    final client = HttpClient();
+    client.connectionTimeout = timeOutDuration;
+    final request = await client.post(
+        soapRequest.serviceEndpointConfiguration.host,
+        soapRequest.serviceEndpointConfiguration.port,
+        soapRequest.serviceEndpointConfiguration.path);
+    request.headers.contentType =
+        ContentType('application', 'soap+xml', charset: 'utf-8');
+    request.headers
+        .add('Authorization', authorizationToken, preserveHeaderCase: true);
+    request.write(soapRequest.generateEnvelope());
+
+    final response = await request.close();
+    client.close();
+
+    final content = await _decodeContent(response);
+
+    //This code is added because the service could respond
+    //with an soap:Envelope which could contain additional information
+    //why its a bad request.
+    print(content);
+    if (response.statusCode != HttpStatus.ok) {
+      try {
+        SoapResponse(content);
+      } on SoapException {
+        rethrow;
+      } on LibraryException {
+        throw HttpException(response.statusCode, response.reasonPhrase);
+      }
+    } else {
+      return SoapResponse(content);
+    }
     throw UnimplementedError();
+  }
+
+  ///Decodes content of response.
+  ///
+  /// Throws [FormatException] is response is according to charset specification.
+  Future<String> _decodeContent(HttpClientResponse response) async {
+    try {
+      final typ = response.headers.contentType?.charset;
+      switch (typ) {
+        case 'ascii':
+        case 'us-ascii':
+          return await ascii.decoder.bind(response).join();
+        case 'latin-1':
+        case 'iso-8859-1':
+          return await latin1.decoder.bind(response).join();
+        case 'utf-8':
+        case null:
+        default:
+          return await utf8.decoder.bind(response).join();
+      }
+    } on FormatException catch (exception) {
+      throw StringDecodingException(exception);
+    }
   }
 }
 
