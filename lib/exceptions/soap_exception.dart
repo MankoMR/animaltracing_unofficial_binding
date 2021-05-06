c89ba5d0a163a82005947b740a40067015b3e5a3
MankoMR <manuel.koloska@gmail.com>
2021-05-06 14:54:26 +0200

    Make errorCode optional.

--- lib/exceptions/soap_exception.dart
+++ lib/exceptions/soap_exception.dart
@@ -7,7 +7,7 @@ import 'library_exception.dart';
 
 ///Gets thrown when soap:Envelope contains a soap:Error.
 class SoapException implements LibraryException {
-  final String errorCode;
+  final String? errorCode;
   final String message;
 
   SoapException(this.errorCode, this.message);
