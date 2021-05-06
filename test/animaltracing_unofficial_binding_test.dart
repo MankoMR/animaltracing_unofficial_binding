881b951f7c656d25572c25a09f0a90baf31ac1f4
MankoMR <manuel.koloska@gmail.com>
2021-05-06 14:56:14 +0200

    Add copyright-notice

--- test/animaltracing_unofficial_binding_test.dart
+++ test/animaltracing_unofficial_binding_test.dart
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021, Manuel Koloska. All Rights reserved.
+ * Filename: animaltracing_unofficial_binding_test.dart
+ * Project: animaltracing_unofficial_binding.
+ */
 import 'package:test/test.dart';
 
 void main() {
