d611cc535faeee8ee4465bbe377735c26e9f3e25
MankoMR <manuel.koloska@gmail.com>
2021-05-06 14:52:20 +0200

    Implement SoapRequest.

--- lib/src/soap/soap_request.dart
+++ lib/src/soap/soap_request.dart
@@ -3,6 +3,9 @@
  * Filename: soap_request.dart
  * Project: animaltracing_unofficial_binding.
  */
+import 'package:animaltracing_unofficial_binding/src/xml_utils.dart';
+import 'package:xml/xml.dart';
+
 import '../../core/core.dart';
 
 class SoapRequest {
@@ -13,6 +16,21 @@ class SoapRequest {
   SoapRequest(this.serviceEndPoint, this.serviceOperation, this.requestData);
 
   String generateEnvelope() {
-    throw UnimplementedError();
+    final builder = XmlBuilder(optimizeNamespaces: true);
+
+    builder.element('Envelope',
+        namespace: soapNameSpace, namespaces: nameSpaceMapping, nest: () {
+      builder.element('Header', namespace: soapNameSpace, nest: () {
+        builder.element('Action',
+            namespace: adressingNameSpace, nest: serviceOperation);
+        builder.element('To',
+            namespace: adressingNameSpace, nest: serviceEndPoint);
+      });
+      builder.element('Body', namespace: soapNameSpace, nest: () {
+        requestData.generateWith(builder, null);
+      });
+    });
+    //TODO: Revisit when text from User is sent. There could be problems with whitespace.
+    return builder.buildDocument().toXmlString(pretty: true);
   }
 }
---
+++ lib/src/xml_utils.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Manuel Koloska. All Rights reserved.
+ * Filename: xml_utils.dart
+ * Project: animaltracing_unofficial_binding.
+ */
+
+const String soapNameSpace = 'http://www.w3.org/2003/05/soap-envelope/';
+const String adressingNameSpace = 'http://www.w3.org/2005/08/addressing';
+
+const nameSpaceMapping = {
+  soapNameSpace: 'soap',
+  adressingNameSpace: 'wsa',
+};
