41fa3b96e4d41f9fc7f35996116c12af0fb02e6a
MankoMR <manuel.koloska@gmail.com>
2021-05-07 15:40:44 +0200

    Add tests for IOClient

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -17,3 +17,27 @@ class MockRequestData extends RequestData {
   }
 }
 
+/// Signature to handle a request
+///
+/// The implementer is responsible to call request.response.close() .
+typedef RequestHandler = Future<void> Function(HttpRequest request);
+
+/// Portnumber on which a testserver listens.
+const _portNumber = 4041;
+
+/// ServiceEndpointConfiguration used to connect temporary test servers. See [createServer]
+final testServerConfiguration =
+    ServiceEndpointConfiguration('localhost', _portNumber, 'test');
+
+/// Sets up a Server with the following Configuration:
+/// IPAddress: loopbackIPv4, port: 4041
+///
+/// [HttpRequest]s need to be handled by [requestHandler].
+Future<HttpServer> createServer(RequestHandler requestHandler) async {
+  final server =
+      await HttpServer.bind(InternetAddress.loopbackIPv4, _portNumber);
+  server.listen((request) {
+    requestHandler(request);
+  });
+  return server;
+}
