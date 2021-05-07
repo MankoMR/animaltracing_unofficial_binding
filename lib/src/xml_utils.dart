c4bc78da8af618eb9b563dc11b4d386b271e4b9f
MankoMR <manuel.koloska@gmail.com>
2021-05-07 17:56:46 +0200

    Add utility function

--- lib/src/xml_utils.dart
+++ lib/src/xml_utils.dart
@@ -4,6 +4,8 @@
  * Project: animaltracing_unofficial_binding.
  */
 
+import 'package:xml/xml.dart';
+
 const String soapNameSpace = 'http://www.w3.org/2003/05/soap-envelope';
 const String adressingNameSpace = 'http://www.w3.org/2005/08/addressing';
 
@@ -11,3 +13,56 @@ const nameSpaceMapping = {
   soapNameSpace: 'soap',
   adressingNameSpace: 'wsa',
 };
+
+void buildNullableElement(
+  XmlBuilder builder,
+  String elementName,
+  String namespace,
+  NullabilityType nullabilityType,
+  Object? value,
+) {
+  if (value == null) {
+    builder.element(
+      elementName,
+      namespace: namespace,
+      nest: () {
+        builder.attribute('isNill', 'true');
+      },
+    );
+  } else {
+    builder.element(elementName, namespace: namespace, nest: value);
+  }
+}
+
+/*
+typedef ItemBuilder<T> = void Function(XmlBuilder builder, T value);
+void buildList<T>(
+  XmlBuilder builder,
+  String elementName,
+  String namespace,
+  NullabilityType nullabilityType,
+  List<T>? values,
+  ItemBuilder<T> itemBuilder,
+) {
+  if (values == null) {
+    builder.element(
+      elementName,
+      namespace: namespace,
+      nest: () {
+        builder.attribute('isNill', 'true');
+      },
+    );
+  } else {
+    builder.element(elementName, namespace: namespace, nest: () {
+      for (final value in values) {
+        itemBuilder(builder, value);
+      }
+    });
+  }
+}
+ */
+
+enum NullabilityType {
+  optionalElement,
+  nullable,
+}
